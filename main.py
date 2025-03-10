7193035abbebf5ed53790cbf23284a2d5cf343b6
Michael Bolden <103343331+MiLkEE44@users.noreply.github.com>
2025-03-09 23:15:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 # Input from the user. 
-print("Welcome to the PyPassword Generator!")
+print("How long do you want your password to be?")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-# string where the password where be. 
+# string where the password where be store. 
 password = []
 
 # Loops where the range is between 0 and the user input. 
